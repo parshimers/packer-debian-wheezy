fdc5b812f18586fbb38dc58b458f5df68cbc3474
Jason Hsu <jhsu@crunchbang.Home>
2015-02-02 23:06:32 -0600

    peer method for postgres user, md5 method for all users

--- scripts/root/postgresql.sh
+++ scripts/root/postgresql.sh
@@ -50,8 +50,32 @@ PG_DIR="/var/lib/postgresql/$PG_VERSION/main"
 # Edit postgresql.conf to change listen address to '*':
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
 
-# Append to pg_hba.conf to add password auth:
+# Save copy of original postgresql.conf file first
+cp $PG_HBA /etc/postgresql/$PG_VERSION/main/pg_hba_orig.conf
+chown postgres:postgres /etc/postgresql/$PG_VERSION/main/pg_hba_orig.conf
+
+# Create new postgresql.conf file
+echo "# Database administrative login by Unix domain socket" > "$PG_HBA"
+echo "local   all             postgres                                peer" >> "$PG_HBA"
+echo "" >> "$PG_HBA"
+echo "# TYPE  DATABASE        USER            ADDRESS                 METHOD" >> "$PG_HBA"
+echo "" >> "$PG_HBA"
+
+# Changed from original file: md5 instead of peer
+echo "# local is for Unix domain socket connections only" >> "$PG_HBA"
+echo "local   all             all                                     md5" >> "$PG_HBA"
+
+echo "" >> "$PG_HBA"
+echo "# IPv4 local connections:" >> "$PG_HBA"
+echo "host    all             all             127.0.0.1/32            md5" >> "$PG_HBA"
+echo "" >> "$PG_HBA"
+echo "# IPv6 local connections:" >> "$PG_HBA"
+echo "host    all             all             ::1/128                 md5" >> "$PG_HBA"
+echo "" >> "$PG_HBA"
+
+# Not included in original file: append to pg_hba.conf to add password auth:
 echo "host    all             all             all                     md5" >> "$PG_HBA"
+chown postgres:postgres $PG_HBA
 
 # Explicitly set default client_encoding
 echo "client_encoding = utf8" >> "$PG_CONF"
